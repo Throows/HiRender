a9a51cffad9d606b36f1c38621a1265b77e4bb8c
Throows <throowsdev@gmail.com>
2021-08-07 12:13:31 +0200

    Fix mutliple project

---
+++ GenerateProject.bat
@@ -0,0 +1,3 @@
+cd C:\dev\HiRender
+call xmake project -k vsxmake -m "debug;release"
+pause
--- xmake.lua
+++ xmake.lua
@@ -1,20 +1,22 @@
 add_rules("mode.debug", "mode.release")
 
-add_requires("spdlog", "glfw", "vulkan-tools", "glad","glm")
+add_requires("spdlog", "glfw", "vulkan-tools", "glad", "glm")
 
 set_runtimes("MD")
 set_languages("cxx17")
+
+set_project("HiRender")
 
 target("HiRenderVulkan")
     set_version("0.0.1")
     set_kind("binary")
 
-    add_files("src/vulkan/*.cpp")
-    add_headerfiles("src/vulkan/*.h")
-    add_includedirs("include/")
-    add_includedirs("shaders/")
+    add_files("src/HiRender/vulkan/*.cpp")
+    add_headerfiles("src/HiRender/vulkan/*.h")
+
     add_packages("spdlog", "glfw", "vulkan-tools", "glm") 
     add_syslinks("vulkan-1")
+
     set_symbols("debug")
     after_build(function (target) 
         -- TODO impl to move /shaders
@@ -27,12 +29,12 @@ target_end()
 target("HiRenderOpenGL")
     set_version("0.0.1")
     set_kind("binary")
+    
+    add_files("src/HiRender/opengl/*.cpp")
+    add_headerfiles("src/HiRender/opengl/*.h")
 
-    add_files("src/opengl/*.cpp")
-    add_headerfiles("src/opengl/*.h")
-    add_includedirs("include/")
-    add_includedirs("shaders/")
     add_packages("spdlog", "glfw", "glad", "glm") 
-    add_syslinks("")
+    add_syslinks("opengl32")
+    
     set_symbols("debug")
 target_end()
